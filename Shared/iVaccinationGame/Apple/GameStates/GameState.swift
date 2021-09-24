ccdf5215cb139aa66b39560743c45c254e013f0e
jetedonner <kim@kimhauser.ch>
2021-09-24 20:53:57 +0200

    Extension for GameSceneBase

---
+++ Shared/iVaccinationGame/Apple/GameStates/GameState.swift
@@ -0,0 +1,19 @@
+//
+//  GameState.swift
+//  iVaccinationGame
+//
+//  Created by Kim David Hauser on 24.09.21.
+//
+
+import Foundation
+
+enum GameState:Int{
+    case gameLoading
+    case gameRunning
+    case paused
+    case menuShowing
+    case settings
+    case gameOver
+    case gameCenterShowing
+    case highscoreShowing
+}
