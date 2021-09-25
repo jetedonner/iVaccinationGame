c335c4c59e5a186e57509da226fbb6f8236e2f2f
jetedonner <kim@kimhauser.ch>
2021-09-25 18:45:41 +0200

    Before enabling iCloud and saving game state

---
+++ iOS/iVaccination/SpriteKit/DifficultyScene.swift
@@ -0,0 +1,37 @@
+//
+//  MenuScene.swift
+//  iVaccination
+//
+//  Created by Kim David Hauser on 25.09.21.
+//
+
+import Foundation
+import SpriteKit
+
+class DifficultyScene: DifficultySceneBase {
+    
+    override func touchOrClick(pos: CGPoint) {
+        if let menuScene = (self.scene as? MenuScene){
+            
+            let node = self.atPoint(pos)
+            
+            if(node == lblEasy){
+                if let viewCtrl = self.view?.window?.contentViewController{
+                    (viewCtrl as! ViewController).loadGameScene(difficulty: .easy, level: self.selLevel)
+                }
+            }else if(node == lblMedium){
+                if let viewCtrl = self.view?.window?.contentViewController{
+                    (viewCtrl as! ViewController).loadGameScene(difficulty: .medium, level: self.selLevel)
+                }
+            }else if(node == lblHard){
+                if let viewCtrl = self.view?.window?.contentViewController{
+                    (viewCtrl as! ViewController).loadGameScene(difficulty: .hard, level: self.selLevel)
+                }
+            }else if(node == lblNightmare){
+                if let viewCtrl = self.view?.window?.contentViewController{
+                    (viewCtrl as! ViewController).loadGameScene(difficulty: .nightmare, level: self.selLevel)
+                }
+            }
+        }
+    }
+}
---
+++ macOS/iVaccinationGame/SpriteKit/DescriptionScene.swift
@@ -0,0 +1,44 @@
+//
+//  DescriptionScene.swift
+//  iVaccinationGame
+//
+//  Created by Kim David Hauser on 25.09.21.
+//
+
+import Foundation
+import SpriteKit
+
+class DescriptionScene: DescriptionSceneBase {
+    
+    override func mouseMoved(with event: NSEvent) {
+        super.mouseMoved(with: event)
+        let location = event.location(in: self.sceneNode)
+        let node = self.scene!.atPoint(location)
+        var newSelNode:SKLabelNode?
+
+        if(node == self.lblContinue){
+            newSelNode = self.lblContinue
+        }else{
+            return
+        }
+
+        if(newSelNode != self.selNode){
+            self.selNode = newSelNode
+            if(self.selNode == self.lblContinue){
+                
+            }
+
+            if(UserDefaultsHelper.playSounds){
+                self.selNode?.run(SoundManager.menuHighliteSound)
+            }
+        }
+    }
+    
+    override func touchOrClick(pos: CGPoint) {
+        if(self.selNode == lblContinue){
+            if let viewCtrl = self.view?.window?.contentViewController{
+                (viewCtrl as! ViewController).loadGameScene(difficulty: .easy)
+            }
+        }
+    }
+}
